b56e5ae63325b63b420ad3fd3d2e947276fa3da3
maulen <example@example.com>
2023-09-09 11:27:03 +0600

    Основы Git: Откат изменений

--- src/main/java/ru/job4j/threads/Main.java
+++ src/main/java/ru/job4j/threads/Main.java
@@ -3,5 +3,9 @@ package ru.job4j.threads;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
