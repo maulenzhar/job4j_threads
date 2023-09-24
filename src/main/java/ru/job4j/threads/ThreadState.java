b0d640ffce51c9171a3f792ad4b932400433f291
maulen <example@example.com>
2023-09-25 00:10:45 +0600

    Threads:  Состояние нити

--- src/main/java/ru/job4j/threads/ThreadState.java
+++ src/main/java/ru/job4j/threads/ThreadState.java
@@ -17,7 +17,7 @@ public class ThreadState {
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState());
         }
-        if (first.getState() == Thread.State.TERMINATED || second.getState() == Thread.State.TERMINATED ) {
+        if (first.getState() == Thread.State.TERMINATED || second.getState() == Thread.State.TERMINATED) {
             System.out.println("работа завершена");
         }
 
