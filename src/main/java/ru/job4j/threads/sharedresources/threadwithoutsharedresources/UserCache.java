b270093071169dec9b2399f79ed663adab0d8df0
maulen <example@example.com>
2023-10-19 10:19:40 +0600

    Threads: Thread без общих ресурсов

--- src/main/java/ru/job4j/threads/sharedresources/threadwithoutsharedresources/UserCache.java
+++ src/main/java/ru/job4j/threads/sharedresources/threadwithoutsharedresources/UserCache.java
@@ -10,87 +10,24 @@ public class UserCache {
     private final AtomicInteger id = new AtomicInteger();
 
     public void add(User user) {
-        /*users.put(id.incrementAndGet(), user);*/
         users.put(id.incrementAndGet(), User.of(user.getName()));
 
     }
 
     public User findById(int id) {
-        /*return users.get(id);*/
         return User.of(users.get(id).getName());
     }
 
     public List<User> findAll() {
-        /*return new ArrayList<>(users.values());*/
         List list = new ArrayList();
 
         for (User user : users.values()) {
-            list.add(findById(user.getId() + 1));
+            list.add(User.of(user.getName()));
         }
 
         return list;
     }
 }
 
-class User {
-    private int id;
-    private String name;
 
-    public static User of(String name) {
-        User user = new User();
-        user.name = name;
-        return user;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + '}';
-    }
-}
-
-class ShareNotSafe {
-    public static void main(String[] args) throws InterruptedException {
-        UserCache cache = new UserCache();
-        User user = User.of("main");
-        cache.add(user);
-        Thread first = new Thread(
-                () -> {
-                    for (int i = 0; i < 100; i++) {
-                        user.setName("first");
-                    }
-                }
-        );
-        Thread second = new Thread(
-                () -> {
-                    for (int i = 0; i < 100; i++) {
-                        user.setName("second");
-                    }
-                }
-        );
-        first.start();
-        second.start();
-        first.join();
-        second.join();
-        System.out.println(cache.findById(1).getName());
-        System.out.println(cache.findAll());
-    }
-}
