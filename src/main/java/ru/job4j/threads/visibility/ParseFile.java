165bddbe506d019830da8b2c424562e82adf6c5b
maulen <example@example.com>
2023-10-30 21:27:59 +0600

    Threads: isibility. Общий ресурс вне критической секции

--- src/main/java/ru/job4j/threads/visibility/ParseFile.java
+++ src/main/java/ru/job4j/threads/visibility/ParseFile.java
@@ -5,33 +5,34 @@ import java.util.function.Predicate;
 
 public final class ParseFile {
     private final File file;
-
+    private Predicate<Integer> pred = c -> c > 0;
+    private int data;
     public ParseFile(File file) {
         synchronized (this) {
             this.file = file;
         }
     }
 
-    public String getContent() throws IOException {
+    public synchronized String getContent() throws IOException {
         try (InputStream i = new BufferedInputStream(new FileInputStream(file))) {
             String output = "";
-            int data;
             data = i.read();
-            while (content(c -> c > 0, data)) {
+            while (content(pred)) {
                 output += (char) data;
+                data = i.read();
             }
             return output;
         }
     }
 
-    public String getContentWithoutUnicode() {
+    public synchronized String getContentWithoutUnicode() {
         try (InputStream i = new BufferedInputStream(new FileInputStream(file))) {
         String output = "";
-        int data;
         data = i.read();
-        while (content(c -> c > 0, data)) {
-            if (data < 0x80) {
+        while (content(pred)) {
+            if (content(c -> c < 0x80)) {
                 output += (char) data;
+                data = i.read();
             }
         }
         return output;
@@ -41,7 +42,13 @@ public final class ParseFile {
         }
     }
 
-    public boolean content(Predicate<Integer> filter, int c) {
-        return filter.test(c);
+    public boolean content(Predicate<Integer> filter) {
+        return filter.test(data);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ParseFile parseFile = new ParseFile(new File("/Users/maulen/Documents/1.Projects/projects/job4j_design2/log.txt"));
+        System.out.println(parseFile.getContent());
+        System.out.println(parseFile.getContentWithoutUnicode());
     }
 }
