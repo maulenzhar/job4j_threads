2fa3a4fae39001ad5f111fd5d5fbf62eed9c20f6
m_zharylgasynov <Maulen.Zharylgasynov@cbk.kg>
2025-05-19 19:05:14 +0600

    Обеспечить остановку потребителя

---
+++ src/main/java/ru/job4j/threads/waitnotifyall/wait/hw/parallelsearch/ParallelSearch.java
@@ -0,0 +1,44 @@
+package ru.job4j.threads.waitnotifyall.wait.hw.parallelsearch;
+
+import ru.job4j.threads.waitnotifyall.wait.hw.simpleblockingqueue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        consumer.interrupt();
+    }
+}
