ac77ec2806933854d96fe26222c92000cf97c347
m_zharylgasynov <Maulen.Zharylgasynov@cbk.kg>
2025-05-26 18:51:36 +0600

    CAS - операции

--- src/main/java/ru/job4j/threads/cas/hw/CASCount.java
+++ src/main/java/ru/job4j/threads/cas/hw/CASCount.java
@@ -1,9 +1,7 @@
 package ru.job4j.threads.cas.hw;
 
 import net.jcip.annotations.ThreadSafe;
-import ru.job4j.threads.cas.CASStack;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
@@ -11,10 +9,24 @@ public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>();
 
     public void increment() {
+        Integer ref;
+        Integer temp;
 
+        do {
+            ref = count.get();
+            if (ref == null) {
+                throw new UnsupportedOperationException("Count is not impl.");
+            }
+            temp = ref + 1;
+        } while (!count.compareAndSet(ref, temp));
     }
 
     public int get() {
-
+        Integer ref = count.get();
+        if (ref == null) {
+            throw new UnsupportedOperationException("Count is not impl.");
+        }
+        return ref;
     }
+
 }
