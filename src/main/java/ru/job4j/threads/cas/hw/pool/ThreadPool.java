4f90c3d2e0fbd694c0e77cbe870d1108f31d5197
m_zharylgasynov <Maulen.Zharylgasynov@cbk.kg>
2025-06-03 16:30:06 +0600

    реализация ThreadPool

---
+++ src/main/java/ru/job4j/threads/cas/hw/pool/ThreadPool.java
@@ -0,0 +1,62 @@
+package ru.job4j.threads.cas.hw.pool;
+
+import ru.job4j.threads.waitnotifyall.wait.hw.simpleblockingqueue.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+
+    private List<Thread> threads = null;
+    private SimpleBlockingQueue<Runnable> tasks = null;
+
+    public ThreadPool(int size) {
+        threads = new LinkedList<>();
+        tasks = new SimpleBlockingQueue<>(size);
+
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread(() -> {
+                        try {
+                            Runnable poll = tasks.poll();
+                            poll.run();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    )
+            );
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutDown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = Runtime.getRuntime().availableProcessors();
+        ThreadPool threadPool = new ThreadPool(size);
+
+        for (int i = 0; i < 10; i++) {
+            final int jobId = i;
+            threadPool.work(() -> {
+                System.out.println("Job " + jobId + " executed by " + Thread.currentThread().getName());
+            });
+        }
+
+        threadPool.shutDown();
+    }
+}
