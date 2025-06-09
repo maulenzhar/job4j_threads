bd0f6fcb174ad7484be3dece3a30c942ede5b8f7
m_zharylgasynov <Maulen.Zharylgasynov@cbk.kg>
2025-06-09 17:26:02 +0600

    ExecutorService рассылка почты

---
+++ src/main/java/ru/job4j/threads/cas/hw/emailnotification/EmailNotification.java
@@ -0,0 +1,38 @@
+package ru.job4j.threads.cas.hw.emailnotification;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
+
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+        System.out.println("Выполняется задача [" + subject + "] потоком " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        emailNotification.emailTo(new User("test", "test@test.ru"));
+        emailNotification.close();
+    }
+}
