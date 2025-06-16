e7d089058d85c11793d03d166b13d54a335da667
m_zharylgasynov <Maulen.Zharylgasynov@cbk.kg>
2025-06-16 18:44:45 +0600

    ForkJoinPool

--- src/main/java/ru/job4j/threads/threadpools/forkjoinpool/hw/SumTask.java
+++ src/main/java/ru/job4j/threads/threadpools/forkjoinpool/hw/SumTask.java
@@ -32,7 +32,9 @@ public class SumTask extends RecursiveTask<Long> {
 
     public static void main(String[] args) {
         int[] array = new int[100];
-        for (int i = 0; i < array.length; i++) array[i] = i + 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
 
         ForkJoinPool pool = new ForkJoinPool();
         SumTask task = new SumTask(array, 0, array.length);
